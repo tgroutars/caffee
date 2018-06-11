8a7ee797c5c6f8cc9b98ef530b7b634525e0a79d
tgroutars <tgroutars@gmail.com>
2018-06-11 21:45:27 +0200

    Refactor messages

--- server/integrations/slack/messages/messages/index.js
+++ server/integrations/slack/messages/messages/index.js
@@ -1,5 +1,18 @@
-const feedbackChooseProduct = require('./feedbackChooseProduct');
+/* eslint-disable global-require,import/no-dynamic-require */
 
-module.exports = {
-  feedback_choose_product: feedbackChooseProduct,
-};
+const fs = require('fs');
+const path = require('path');
+
+const messages = {};
+
+const basename = path.basename(module.filename);
+
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== basename)
+  .forEach(file => {
+    const message = require(`./${file}`);
+    const type = file.split('.')[0];
+    messages[type] = message;
+  });
+
+module.exports = messages;

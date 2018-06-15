828cc0eced21afe7368a3ee4bb57593b992e3a31
tgroutars <tgroutars@gmail.com>
2018-06-15 17:04:17 +0200

    Remove unused koa-static

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 require('events').EventEmitter.defaultMaxListeners = 25;
 
-const path = require('path');
 const http = require('http');
 
 const Koa = require('koa');
 const enforceHttps = require('koa-sslify');
 const koaLogger = require('koa-logger');
 const koaBodyParser = require('koa-bodyparser');
-const koaStatic = require('koa-static');
 
 const { init: initEventListeners } = require('./eventQueue/listeners');
 const router = require('./router');
@@ -30,9 +28,6 @@ if (process.env.NODE_ENV !== 'test') {
   app.use(koaLogger());
 }
 app.use(koaBodyParser());
-
-app.use(koaStatic(path.join(__dirname, '../dist')));
-app.use(koaStatic(path.join(__dirname, '../public')));
 
 // Routes
 app.use(router.routes());

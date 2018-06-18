b424c69c7c721369da8a0584dcf3541a6a254d64
tgroutars <tgroutars@gmail.com>
2018-06-18 11:42:29 +0200

    Remove console.log s

--- server/eventQueue/listeners/backlog_item_moved.js
+++ server/eventQueue/listeners/backlog_item_moved.js
@@ -1,7 +1,8 @@
-const backlogItemMoved = async ({ backlogItemId, oldList, newList }) => {
-  console.log(backlogItemId);
-  console.log(oldList);
-  console.log(newList);
+const backlogItemMoved = async (/* { backlogItemId, oldList, newList } */) => {
+  // console.log(backlogItemId);
+  // console.log(oldList);
+  // console.log(newList);
+  // TODO: Send notifications
 };
 
 module.exports = backlogItemMoved;

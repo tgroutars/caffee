c11af245463f12d22ec7e20c434505acc1933295
tgroutars <tgroutars@gmail.com>
2018-06-22 22:45:21 +0200

    Search users

--- server/integrations/slack/options-load/dialogSuggestion/elements/feedback-authorId.js
+++ server/integrations/slack/options-load/dialogSuggestion/elements/feedback-authorId.js
@@ -1,12 +1,25 @@
-const { SlackWorkspace } = require('../../../../../models');
+const {
+  SlackWorkspace,
+  SlackUser,
+  Sequelize,
+} = require('../../../../../models');
+
+const { Op } = Sequelize;
 
 const feedbackAuthorId = async payload => {
   const {
     team: { id: workspaceSlackId },
+    value,
   } = payload;
   const workspace = await SlackWorkspace.find({
     where: { slackId: workspaceSlackId },
-    include: ['slackUsers'],
+    include: [
+      {
+        model: SlackUser,
+        as: 'slackUsers',
+        where: { name: { [Op.iLike]: `%${value.replace('%', '\\%')}%` } },
+      },
+    ],
   });
   const { slackUsers } = workspace;
   return slackUsers.map(slackUser => ({

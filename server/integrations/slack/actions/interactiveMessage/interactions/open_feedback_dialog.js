07ca2a8d1e508917c7ea860dcc9449915d5c4ca3
tgroutars <tgroutars@gmail.com>
2018-06-22 22:56:56 +0200

    Select Author for users when talking to Caffee

--- server/integrations/slack/actions/interactiveMessage/interactions/open_feedback_dialog.js
+++ server/integrations/slack/actions/interactiveMessage/interactions/open_feedback_dialog.js
@@ -1,22 +1,52 @@
-const { SlackWorkspace } = require('../../../../../models');
+const {
+  SlackWorkspace,
+  ProductUser,
+  SlackUser,
+  Sequelize,
+} = require('../../../../../models');
 const { openDialog } = require('../../../dialogs');
+
+const { Op } = Sequelize;
 
 const openFeedbackDialogHelper = openDialog('feedback');
 
 const openFeedbackDialog = async payload => {
   const {
     team: { id: workspaceSlackId },
+    user: { id: userSlackId },
     trigger_id: triggerId,
     action,
   } = payload;
 
   const { productId, defaultFeedback } = action.name;
+
+  const slackUser = await SlackUser.find({
+    where: { slackId: userSlackId },
+    include: [
+      {
+        model: SlackWorkspace,
+        as: 'workspace',
+        where: { slackId: workspaceSlackId },
+      },
+    ],
+  });
   const workspace = await SlackWorkspace.find({
     where: { slackId: workspaceSlackId },
   });
   const { accessToken } = workspace;
 
-  await openFeedbackDialogHelper({ productId, defaultFeedback })({
+  const productUser = await ProductUser.find({
+    where: {
+      userId: slackUser.userId,
+      productId,
+      role: { [Op.in]: ['user', 'admin'] },
+    },
+  });
+  await openFeedbackDialogHelper({
+    productId,
+    defaultFeedback,
+    selectAuthor: !!productUser,
+  })({
     accessToken,
     triggerId,
   });

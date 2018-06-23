1398be259a50730d531748e8321e6ef0c3e46259
tgroutars <tgroutars@gmail.com>
2018-06-23 11:56:36 +0200

    Also search in backlog item description

--- server/integrations/slack/options-load/interactiveMessage/actions/add_feedback_to_backlog_item.js
+++ server/integrations/slack/options-load/interactiveMessage/actions/add_feedback_to_backlog_item.js
@@ -6,10 +6,15 @@ const addFeedbackToBacklogItem = async payload => {
   const { value, name } = payload;
   const { productId } = name;
 
+  const searchString = `%${value.replace('%', '\\%')}%`;
+
   const backlogItems = await BacklogItem.findAll({
     where: {
       productId,
-      [Op.or]: { title: { [Op.iLike]: `%${value.replace('%', '\\%')}%` } },
+      [Op.or]: {
+        title: { [Op.iLike]: searchString },
+        description: { [Op.iLike]: searchString },
+      },
     },
   });
 

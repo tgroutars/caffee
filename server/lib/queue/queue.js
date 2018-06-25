3548d0b61c62b6edafb9d6e758a24b77e9cb214a
tgroutars <tgroutars@gmail.com>
2018-06-26 00:18:31 +0200

    Remove failed jobs

--- server/lib/queue/queue.js
+++ server/lib/queue/queue.js
@@ -9,6 +9,17 @@ const queue = kue.createQueue({
   },
 });
 
+// Remove failed jobs every hour
+setInterval(() => {
+  kue.Job.rangeByState('failed', 0, 1000, 1, (err, jobs) => {
+    jobs.forEach(job => {
+      if (job.created_at < Date.now() - 7 * 24 * 60 * 60 * 1000) {
+        job.remove();
+      }
+    });
+  });
+}, 1 * 60 * 1000);
+
 if (process.env.NODE_ENV === 'development') {
   kue.app.listen(4000);
 }

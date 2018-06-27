b90fe6c7e69c6f193be8e69d1cc5c68db5551545
tgroutars <tgroutars@gmail.com>
2018-06-27 14:10:38 +0200

    Improve forbidden message

--- server/integrations/slack/messages/messages/forbidden.js
+++ server/integrations/slack/messages/messages/forbidden.js
@@ -1,5 +1,5 @@
 const forbidden = () => ({
-  text: `Sorry, you can't do that. Contact an admin if you think you should`,
+  text: `Sorry, you're not allowed to do that :no_mouth: Contact an admin if you think you should be`,
 });
 
 module.exports = forbidden;

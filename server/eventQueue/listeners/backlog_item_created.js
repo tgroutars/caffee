0ae31e5b87d7cbccc7d7268fcbaa5fb1a05595d0
tgroutars <tgroutars@gmail.com>
2018-07-04 17:49:56 +0200

    Do not send new backlog item to PMs

--- server/eventQueue/listeners/backlog_item_created.js
+++ server/eventQueue/listeners/backlog_item_created.js
@@ -1,14 +1,6 @@
 const Promise = require('bluebird');
-const winston = require('winston');
 
-const {
-  BacklogItem,
-  SlackUser,
-  SlackInstall,
-  User,
-  ProductUser,
-  Sequelize,
-} = require('../../models');
+const { BacklogItem, SlackInstall, Sequelize } = require('../../models');
 const { postMessage } = require('../../integrations/slack/messages');
 
 const { Op } = Sequelize;
@@ -20,36 +12,6 @@ const backlogItemCreated = async ({ backlogItemId }) => {
     include: ['product'],
   });
   const { product } = backlogItem;
-  const productUsers = await ProductUser.findAll({
-    where: { productId: product.id, role: { [Op.in]: ['user', 'admin'] } },
-    include: [{ model: User, as: 'user' }],
-  });
-  const users = productUsers.map(({ user }) => user);
-  const postPrivateNewBacklogItemMessage = postNewBacklogItemMessage({
-    backlogItem,
-    product,
-    suggestFollowers: true,
-    openCard: true,
-    follow: false,
-  });
-  await Promise.map(users, async user => {
-    const slackUsers = await SlackUser.findAll({
-      where: { userId: user.id },
-      include: ['workspace'],
-    });
-    await Promise.map(slackUsers, async slackUser => {
-      const { workspace, slackId } = slackUser;
-      const { accessToken } = workspace;
-      try {
-        await postPrivateNewBacklogItemMessage({
-          accessToken,
-          channel: slackId,
-        });
-      } catch (err) {
-        winston.error(err);
-      }
-    });
-  });
 
   const postPublicNewBacklogItemMessage = postNewBacklogItemMessage({
     backlogItem,

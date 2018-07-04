004c933b7274816b2957950084c81bff1f7fb5c4
tgroutars <tgroutars@gmail.com>
2018-07-04 17:32:44 +0200

    Process actions in background to avoid timeouts

--- server/integrations/slack/actions/index.js
+++ server/integrations/slack/actions/index.js
@@ -1,18 +1,47 @@
 const dialogSubmission = require('./dialogSubmission');
 const interactiveMessage = require('./interactiveMessage');
 const messageAction = require('./messageAction');
+const registerBackgroundTask = require('../../../lib/queue/registerBackgroundTask');
+const { SlackUser, SlackWorkspace } = require('../../../models');
 
-const handleAction = async (payload, state) => {
+const handleAction = registerBackgroundTask(async payload => {
+  const {
+    team: { id: workspaceSlackId },
+    user: { id: userSlackId },
+  } = payload;
+
+  const slackUser = await SlackUser.find({
+    where: { slackId: userSlackId },
+    include: [
+      'user',
+      {
+        model: SlackWorkspace,
+        as: 'workspace',
+        where: { slackId: workspaceSlackId },
+      },
+    ],
+  });
+  if (!slackUser) {
+    return;
+  }
+
+  const { workspace, user } = slackUser;
+
+  const state = { workspace, slackUser, user };
+
   switch (payload.type) {
     case 'interactive_message':
-      return interactiveMessage(payload, state);
+      await interactiveMessage(payload, state);
+      break;
     case 'dialog_submission':
-      return dialogSubmission(payload, state);
+      await dialogSubmission(payload, state);
+      break;
     case 'message_action':
-      return messageAction(payload, state);
+      await messageAction(payload, state);
+      break;
     default:
       throw new Error(`Unknown action type: ${payload.type}`);
   }
-};
+});
 
 module.exports = { handleAction };
--- server/router/integrations/slack/actions.js
+++ server/router/integrations/slack/actions.js
@@ -1,7 +1,6 @@
 const Router = require('koa-router');
 const Boom = require('boom');
 
-const { SlackUser, SlackWorkspace } = require('../../../models');
 const { handleAction } = require('../../../integrations/slack/actions');
 
 const { SLACK_VERIFICATION_TOKEN } = process.env;
@@ -23,37 +22,9 @@ const verifyToken = async (ctx, next) => {
 
 router.use(parsePayload, verifyToken);
 
-router.use(async (ctx, next) => {
-  const { payload } = ctx.state;
-  const {
-    team: { id: workspaceSlackId },
-    user: { id: userSlackId },
-  } = payload;
-
-  const slackUser = await SlackUser.find({
-    where: { slackId: userSlackId },
-    include: [
-      'user',
-      {
-        model: SlackWorkspace,
-        as: 'workspace',
-        where: { slackId: workspaceSlackId },
-      },
-    ],
-  });
-  if (!slackUser) {
-    ctx.body = '';
-    return;
-  }
-  ctx.state.slackUser = slackUser;
-  ctx.state.user = slackUser.user;
-  ctx.state.workspace = slackUser.workspace;
-  await next();
-});
-
 router.post('/', async ctx => {
-  const { payload, workspace, slackUser, user } = ctx.state;
-  ctx.body = await handleAction(payload, { workspace, slackUser, user });
+  const { payload } = ctx.state;
+  ctx.body = await handleAction(payload);
 });
 
 module.exports = router;

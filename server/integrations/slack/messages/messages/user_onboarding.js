a22a33c7e2a3981f3dde6fc5cd722b5b98222b85
tgroutars <tgroutars@gmail.com>
2018-07-26 13:44:43 +0300

    Improve onboarding messages

--- server/integrations/slack/messages/messages/user_onboarding.js
+++ server/integrations/slack/messages/messages/user_onboarding.js
@@ -19,7 +19,7 @@ There's actually a 4th way to send a feedback. When you or someone else types a 
 
 *Why should I send feedbacks through Caffee and not just talk to the Product Manager? :thinking_face:*
 Let's say you have a super idea for a new feature. If the product team is willing to consider it, or is already working on it, they'll link it to an item of the roadmap.
-Now let's say your new feature gets released, I'll be able to personally let you know :blush:
+Now when your new feature gets released, I'll be able to personally let you know :blush:
 
 *Can I willingly decide to follow specific items of the roadmap and get notified of their progress?*
 Of course! When you summon me, you'll have the option to 'View the roadmap'. I'll send you the roadmap as a navigable message, and you'll be able to pick any item you want to follow

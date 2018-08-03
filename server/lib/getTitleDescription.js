6dea9634d4cd8081d7b5b2ce5023452f72953d23
tgroutars <tgroutars@gmail.com>
2018-08-03 11:37:04 +0200

    Limit description to 3000 characters

--- server/lib/getTitleDescription.js
+++ server/lib/getTitleDescription.js
@@ -1,7 +1,10 @@
 const trim = require('lodash/trim');
 const truncate = require('lodash/truncate');
 
-const getTitleDescription = (rawText, { maxTitleLength = 150 } = {}) => {
+const getTitleDescription = (
+  rawText,
+  { maxTitleLength = 150, maxDescriptionLength = 3000 } = {},
+) => {
   const text = trim(rawText);
   const textParts = text.split('\n');
   const [fullTitle] = textParts;
@@ -15,6 +18,10 @@ const getTitleDescription = (rawText, { maxTitleLength = 150 } = {}) => {
     });
     const titleRest = fullTitle.substring(title.length - 3);
     description = `...${titleRest}\n\n${description}`;
+    description = truncate(description, {
+      length: maxDescriptionLength,
+      omission: '...',
+    });
   }
   return { title, description };
 };

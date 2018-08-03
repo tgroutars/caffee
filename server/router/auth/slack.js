410e610c7d4cd5c154cf85b9bd8d3024d0d7c5a4
tgroutars <tgroutars@gmail.com>
2018-08-03 15:05:11 +0200

    Add url to re-authorize without installing

--- server/router/auth/slack.js
+++ server/router/auth/slack.js
@@ -21,8 +21,42 @@ const { SLACK_CLIENT_ID, SLACK_CLIENT_SECRET, BASE_URL } = process.env;
 const AUTHORIZATION_URL = `https://slack.com/oauth/authorize`;
 const ACCESS_URL = 'https://slack.com/api/oauth.access';
 const REDIRECT_URI = `${BASE_URL}/auth/slack/install/callback`;
+const AUTHORIZE_REDIRECT_URI = `${BASE_URL}/auth/slack/authorize/callback`;
 
 const router = new Router();
+
+router.get('/authorize', async ctx => {
+  const queryParams = {
+    scope: SCOPES.join(','),
+    redirect_uri: AUTHORIZE_REDIRECT_URI,
+    client_id: SLACK_CLIENT_ID,
+    single_channel: false,
+  };
+  const query = querystring.stringify(queryParams);
+  const authorizeURL = `${AUTHORIZATION_URL}?${query}`;
+  ctx.redirect(authorizeURL);
+});
+
+router.get('/authorize/callback', async ctx => {
+  const { code } = ctx.query;
+  const { data: access } = await axios.get(ACCESS_URL, {
+    params: {
+      code,
+      client_id: SLACK_CLIENT_ID,
+      client_secret: SLACK_CLIENT_SECRET,
+      redirect_uri: AUTHORIZE_REDIRECT_URI,
+    },
+  });
+  if (!access.ok) {
+    ctx.throw(401);
+  }
+
+  const {
+    authorizing_user: { app_home: appHome },
+  } = access;
+
+  ctx.redirect(`https://slack.com/app_redirect?channel=${appHome}`);
+});
 
 router.get('/install', async ctx => {
   const queryParams = {

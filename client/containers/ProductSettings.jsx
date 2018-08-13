18dee8a453f864feda1b146dbbea28e59b3a918a
tgroutars <tgroutars@gmail.com>
2018-08-13 16:18:08 +0200

    Improve Layout

--- client/containers/ProductSettings.jsx
+++ client/containers/ProductSettings.jsx
@@ -10,11 +10,23 @@ const { Sider, Content } = Layout;
 const MenuItem = Menu.Item;
 
 const StyledLayout = styled(Layout)`
-  margin-left: 128px;
-  margin-right: 128px;
+  margin-left: 256px;
+  margin-right: 256px;
   margin-top: 32px;
   background: #fff;
   max-height: calc(100vh - 128px);
+  @media only screen and (max-width: 992px) {
+    margin-left: 128px;
+    margin-right: 128px;
+  }
+  @media only screen and (max-width: 768px) {
+    margin-left: 24px;
+    margin-right: 24px;
+  }
+  @media only screen and (max-width: 576px) {
+    margin-left: 0;
+    margin-right: 0;
+  }
 `;
 const StyledSider = styled(Sider)`
   background: #fff;

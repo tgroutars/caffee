5a5a4eeff242bff83f7ba4ecbbb9385f65adc0b1
tgroutars <tgroutars@gmail.com>
2018-08-17 10:25:03 +0200

    Fix team join: Add user to products

--- server/integrations/slack/events/events/team_join.js
+++ server/integrations/slack/events/events/team_join.js
@@ -1,6 +1,8 @@
 const SlackClient = require('@slack/client').WebClient;
+const Promise = require('bluebird');
 
 const { isUser, getUserVals } = require('../../helpers/user');
+const { SlackInstall } = require('../../../../models');
 const { SlackUser: SlackUserService } = require('../../../../services');
 
 const teamJoin = async (payload, { workspace }) => {
@@ -13,12 +15,18 @@ const teamJoin = async (payload, { workspace }) => {
     return;
   }
   const { email, name, slackId, image } = getUserVals(userInfo);
-  await SlackUserService.findOrCreate({
+  const [slackUser] = await SlackUserService.findOrCreate({
     email,
     name,
     slackId,
     image,
     workspaceId: workspace.id,
+  });
+  const slackInstalls = await SlackInstall.findAll({
+    where: { workspaceId: workspace.id },
+  });
+  await Promise.map(slackInstalls, async ({ productId }) => {
+    await slackUser.user.addProduct(productId);
   });
 };
 

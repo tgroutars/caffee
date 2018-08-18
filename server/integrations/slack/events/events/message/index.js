814f136c9fa8a360f14b53bc7f60918f88d0c479
tgroutars <tgroutars@gmail.com>
2018-08-18 14:20:13 +0200

    Fix unhendled promie rejections

--- server/integrations/slack/events/events/message/index.js
+++ server/integrations/slack/events/events/message/index.js
@@ -1,3 +1,5 @@
+const Promise = require('bluebird');
+
 // TODO: Rename these to make what they do clearer
 const appHome = require('./app_home');
 const channel = require('./channel');
@@ -5,8 +7,7 @@ const feedbackComment = require('./feedbackComment');
 
 const handlers = [appHome, channel, feedbackComment];
 
-const message = async (payload, state) => {
-  handlers.forEach(handler => handler(payload, state));
-};
+const message = async (payload, state) =>
+  Promise.map(handlers, async handler => handler(payload, state));
 
 module.exports = message;

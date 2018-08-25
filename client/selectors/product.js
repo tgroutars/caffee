02481b7b9c32ceb0c8711154d8e9c7c6f32bef06
tgroutars <tgroutars@gmail.com>
2018-08-25 14:09:52 +0200

    Only show products if user is PM

--- client/selectors/product.js
+++ client/selectors/product.js
@@ -4,11 +4,14 @@ const currentProductIdSelector = state => state.product.productId;
 const productIdsSelector = state => state.product.productIds;
 const productMapSelector = state => state.entities.products;
 
+const isPM = product =>
+  product.userRole === 'admin' || product.userRole === 'user';
+
 export const productsSelector = createSelector(
   productIdsSelector,
   productMapSelector,
   (productIds, productMap) =>
-    productIds.map(productId => productMap[productId]),
+    productIds.map(productId => productMap[productId]).filter(isPM),
 );
 
 export const currentProductSelector = createSelector(

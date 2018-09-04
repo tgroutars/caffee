1ca42294deb0548235284189526f7d3a2373082d
tgroutars <tgroutars@gmail.com>
2018-09-04 14:03:36 +0200

    Fix typo

--- server/integrations/slack/messages/messages/feedback_processed.js
+++ server/integrations/slack/messages/messages/feedback_processed.js
@@ -3,7 +3,7 @@ const getRoadmapItemAttachment = require('../attachments/roadmap_item');
 module.exports = ({ feedback, roadmapItem, processedBy, isPM, stage }) => ({
   text: `*_${
     processedBy ? processedBy.name : 'The product team'
-  } has associated your feedback to a item in the roadmap :thumbsup:_*`,
+  } has associated your feedback to an item in the roadmap :thumbsup:_*`,
   attachments: [
     {
       text: feedback.description,

c2444c9ab128d6314034e0400c0c38683c3ac834
tgroutars <tgroutars@gmail.com>
2018-09-08 13:43:17 +0200

    Check permissions on connect_trello

--- server/integrations/slack/actions/interactiveMessage/interactions/connect_trello.js
+++ server/integrations/slack/actions/interactiveMessage/interactions/connect_trello.js
@@ -1,12 +1,21 @@
 const { getInstallURL } = require('../../../../trello/helpers/auth');
 const { postEphemeral } = require('../../../messages');
+const { ProductUser } = require('../../../../../models');
+const { SlackPermissionError } = require('../../../../../lib/errors');
 
-module.exports = async (payload, { slackUser, workspace }) => {
+module.exports = async (payload, { slackUser, workspace, user }) => {
   const {
     action,
     channel: { id: channel },
   } = payload;
   const { productId } = action.name;
+
+  const productUser = await ProductUser.find({
+    where: { productId, userId: user.id },
+  });
+  if (!productUser || !productUser.isAdmin) {
+    throw new SlackPermissionError();
+  }
 
   const { appUserId, domain, accessToken } = workspace;
   const returnTo = `https://${domain}.slack.com/app_redirect?channel=${appUserId}`;

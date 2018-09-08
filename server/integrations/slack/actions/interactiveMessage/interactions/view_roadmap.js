33568f4499b2b0e6cc84deaf0937e04213c61a4e
tgroutars <tgroutars@gmail.com>
2018-09-08 14:12:10 +0200

    Check permissions on view_roadmap

--- server/integrations/slack/actions/interactiveMessage/interactions/view_roadmap.js
+++ server/integrations/slack/actions/interactiveMessage/interactions/view_roadmap.js
@@ -2,6 +2,8 @@ const SlackClient = require('@slack/client').WebClient;
 
 const getRoadmap = require('../../../helpers/getRoadmap');
 const { postMessage, postEphemeral } = require('../../../messages');
+const { ProductUser } = require('../../../../../models');
+const { SlackPermissionError } = require('../../../../../lib/errors');
 
 module.exports = async (payload, { workspace, slackUser, user }) => {
   const {
@@ -9,6 +11,14 @@ module.exports = async (payload, { workspace, slackUser, user }) => {
     channel: { id: channel },
   } = payload;
   const { productId, page = 0 } = action.name;
+
+  const productUser = await ProductUser.find({
+    where: { productId, userId: user.id },
+  });
+  if (!productUser) {
+    throw new SlackPermissionError();
+  }
+
   const { pageCount, roadmapItems, product, stages, isPM } = await getRoadmap(
     productId,
     user.id,

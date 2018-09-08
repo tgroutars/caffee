c6d9073a4b1fb361f31107660eac62da48e95a4c
tgroutars <tgroutars@gmail.com>
2018-09-08 13:54:12 +0200

    Check permissions on open_create_channel_dialog

--- server/integrations/slack/actions/interactiveMessage/interactions/open_create_channel_dialog.js
+++ server/integrations/slack/actions/interactiveMessage/interactions/open_create_channel_dialog.js
@@ -1,11 +1,22 @@
 const { openDialog } = require('../../../dialogs');
+const { SlackInstall, ProductUser } = require('../../../../../models');
+const { SlackPermissionError } = require('../../../../../lib/errors');
 
 const openCreateChannelDialog = openDialog('create_channel');
 
-module.exports = async (payload, { workspace }) => {
+module.exports = async (payload, { workspace, user }) => {
   const { trigger_id: triggerId, action } = payload;
   const { slackInstallId } = action.name;
   const { accessToken } = workspace;
+
+  const slackInstall = await SlackInstall.findById(slackInstallId);
+  const productUser = await ProductUser.find({
+    where: { productId: slackInstall.productId, userId: user.id },
+  });
+  if (!productUser || !productUser.isAdmin) {
+    throw new SlackPermissionError();
+  }
+
   await openCreateChannelDialog({
     slackInstallId,
   })({

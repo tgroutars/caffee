70aa01bda40c5ab9db1ec6f126e5e401240ffef7
tgroutars <tgroutars@gmail.com>
2018-09-08 13:48:03 +0200

    Check permissions on follow_roadmap_item

--- server/integrations/slack/actions/interactiveMessage/interactions/follow_roadmap_item.js
+++ server/integrations/slack/actions/interactiveMessage/interactions/follow_roadmap_item.js
@@ -1,14 +1,24 @@
 const { RoadmapItem: RoadmapItemService } = require('../../../../../services');
-const { RoadmapItem } = require('../../../../../models');
+const { RoadmapItem, ProductUser } = require('../../../../../models');
 const { postEphemeral } = require('../../../messages');
+const { SlackPermissionError } = require('../../../../../lib/errors');
 
-module.exports = async (payload, { workspace, slackUser }) => {
+// TODO: request an invite to the admin if not allowed to follow
+module.exports = async (payload, { workspace, slackUser, user }) => {
   const {
     action,
     channel: { id: channel },
   } = payload;
   const { roadmapItemId } = action.name;
   const roadmapItem = await RoadmapItem.findById(roadmapItemId);
+
+  const productUser = await ProductUser.find({
+    where: { productId: roadmapItem.productId, userId: user.id },
+  });
+  if (!productUser) {
+    throw new SlackPermissionError();
+  }
+
   const { accessToken } = workspace;
   const [, created] = await RoadmapItemService.addFollower(
     roadmapItemId,

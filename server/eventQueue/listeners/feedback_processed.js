2d420160ad0c8017e576935ab3bb373493b76129
tgroutars <tgroutars@gmail.com>
2018-09-10 12:02:01 +0200

    Send feedback processed message in threads

--- server/eventQueue/listeners/feedback_processed.js
+++ server/eventQueue/listeners/feedback_processed.js
@@ -4,7 +4,7 @@ const winston = require('winston');
 const {
   Feedback,
   User,
-  SlackUser,
+  SlackWorkspace,
   ProductUser,
   RoadmapItem,
   FeedbackComment,
@@ -13,76 +13,71 @@ const { RoadmapItem: RoadmapItemService } = require('../../services');
 const { postMessage } = require('../../integrations/slack/messages');
 const { addComment } = require('../../integrations/trello/helpers/api');
 
-// TODO: send the message in threads instead of just to author
 const feedbackProcessed = async ({ feedbackId, processedById }) => {
   const feedback = await Feedback.findById(feedbackId, {
     include: [
       'product',
+      'author',
+      'externalRefs',
       { model: RoadmapItem, as: 'roadmapItem', include: ['stage'] },
-      {
-        model: User,
-        as: 'author',
-        include: [
-          { model: SlackUser, as: 'slackUsers', include: ['workspace'] },
-        ],
-      },
       { model: FeedbackComment, as: 'comments', include: ['author'] },
     ],
   });
-  const { roadmapItem, author, comments, attachments } = feedback;
+  const { roadmapItem, author, comments, attachments, externalRefs } = feedback;
   const { stage } = roadmapItem;
-  const { slackUsers } = author;
+
   const processedBy = await User.findById(processedById);
   const productUser = await ProductUser.find({
     where: { productId: feedback.productId, userId: author.id },
   });
   const { isPM } = productUser;
   const postFeedbackProcessedMessage = postMessage('feedback_processed')({
-    feedback,
     roadmapItem,
     processedBy,
     isPM,
     stage,
   });
 
-  await Promise.map(slackUsers, async slackUser => {
-    const { workspace, slackId: userSlackId } = slackUser;
+  await Promise.map(externalRefs, async externalRef => {
+    const { workspaceId, channel, ts } = externalRef.props;
+    const workspace = await SlackWorkspace.findById(workspaceId);
     const { accessToken } = workspace;
     try {
-      await postFeedbackProcessedMessage({ accessToken, channel: userSlackId });
+      await postFeedbackProcessedMessage({
+        accessToken,
+        channel,
+        threadTS: ts,
+        replyBroadcast: true,
+      });
     } catch (err) {
       winston.error(err);
     }
   });
 
-  if (roadmapItem) {
-    const { product } = feedback;
-    let text = `**New feedback from ${author.name}:**\n\n${
-      feedback.description
-    }`;
-    if (comments.length) {
-      text = `${text}\n\n**Discussion**`;
-      comments.forEach(comment => {
-        let commentText = comment.text;
-        if (comment.attachments.length) {
-          const attachmentLinks = comment.attachments
-            .map(({ name, url }) => `[${name}](${url})`)
-            .join('\n');
-          commentText = `${commentText}\n${attachmentLinks}`;
-        }
-        text = `${text}\n***${comment.author.name}***\n${commentText}`;
-      });
-    }
-    if (attachments.length) {
-      text = `${text}\n\n**Attachments**\n${attachments.map(
-        ({ name, url }) => `[${name}](${url})`,
-      )}`;
-    }
-    await addComment(product.trelloAccessToken, {
-      cardId: roadmapItem.trelloRef,
-      text,
+  const { product } = feedback;
+  let text = `**New feedback from ${author.name}:**\n\n${feedback.description}`;
+  if (comments.length) {
+    text = `${text}\n\n**Discussion**`;
+    comments.forEach(comment => {
+      let commentText = comment.text;
+      if (comment.attachments.length) {
+        const attachmentLinks = comment.attachments
+          .map(({ name, url }) => `[${name}](${url})`)
+          .join('\n');
+        commentText = `${commentText}\n${attachmentLinks}`;
+      }
+      text = `${text}\n***${comment.author.name}***\n${commentText}`;
     });
   }
+  if (attachments.length) {
+    text = `${text}\n\n**Attachments**\n${attachments.map(
+      ({ name, url }) => `[${name}](${url})`,
+    )}`;
+  }
+  await addComment(product.trelloAccessToken, {
+    cardId: roadmapItem.trelloRef,
+    text,
+  });
   await Promise.map(feedback.attachments, async attachment =>
     RoadmapItemService.addAttachmentAndSync(roadmapItem.id, attachment),
   );
--- server/integrations/slack/messages/messages/feedback_processed.js
+++ server/integrations/slack/messages/messages/feedback_processed.js
@@ -1,14 +1,10 @@
 const getRoadmapItemAttachment = require('../attachments/roadmap_item');
 
-module.exports = ({ feedback, roadmapItem, processedBy, isPM, stage }) => ({
+module.exports = ({ roadmapItem, processedBy, isPM, stage }) => ({
   text: `*_${
     processedBy ? processedBy.name : 'The product team'
-  } has associated your feedback to an item in the roadmap :thumbsup:_*`,
+  } has associated this feedback to an item in the roadmap :thumbsup:_*`,
   attachments: [
-    {
-      text: feedback.description,
-      color: '#f2d600',
-    },
     {
       ...getRoadmapItemAttachment({
         roadmapItem,
